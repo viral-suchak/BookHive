94f5ade43711a024288f2f4ee9f9c5d54f6a3316
viral5917 <suchak.v@northeastern.edu>
2025-03-27 22:35:16 -0400

    Initial commit - BookHive project setup

---
+++ src/main/java/com/bookhive/BookHiveApplication.java
@@ -0,0 +1,13 @@
+package com.bookhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookHiveApplication.class, args);
+	}
+
+}
